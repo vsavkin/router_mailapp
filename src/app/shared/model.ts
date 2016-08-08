38a364593584255b82e81b970ab782eef947e12c
vsavkin <avix1000@gmail.com>
2016-08-07 20:29:49 -0700

    wip

--- src/app/shared/model.ts
+++ src/app/shared/model.ts
@@ -26,87 +26,95 @@ export type Message = {
   user: User;
 };
 
+export type AppState = {
+  users: User[];
+  conversations: Conversation[];
+  messages: Message[];
+};
+
 export class Repo {
-  static users = [
-    { id: 0, name: 'Victor', email: 'victors@example.com' },
-    { id: 1, name: 'Kate', email: 'katez@example.com' },
-    { id: 2, name: 'Someone Else', email: 'someoneelse@example.com' }
-  ];
+  state: AppState = {
+    users: [
+      { id: 0, name: 'Victor', email: 'victors@example.com' },
+      { id: 1, name: 'Kate', email: 'katez@example.com' },
+      { id: 2, name: 'Someone Else', email: 'someoneelse@example.com' }
+    ],
 
-  static conversations = [
-    {
-      id: 0,
-      user: Repo.users[0],
-      title: "The Myth of Sisyphus",
-      messages: [0, 1],
-      folder: 'inbox'
-    },
-    {
-      id: 1,
-      user: Repo.users[1],
-      title: "The Nicomachean Ethics",
-      messages: [2, 3],
-      folder: 'inbox'
-    },
-    {
-      id: 2,
-      user: Repo.users[0],
-      title: 'A Fraft Conversation',
-      messages: [4],
-      folder: 'drafts'
-    }
-  ];
+    conversations: [
+      {
+        id: 0,
+        user: this.state.users[0],
+        title: "The Myth of Sisyphus",
+        messages: [0, 1],
+        folder: 'inbox'
+      },
+      {
+        id: 1,
+        user: this.state.users[1],
+        title: "The Nicomachean Ethics",
+        messages: [2, 3],
+        folder: 'inbox'
+      },
+      {
+        id: 2,
+        user: this.state.users[0],
+        title: 'A Fraft Conversation',
+        messages: [4],
+        folder: 'drafts'
+      }
+    ],
 
-  static messages = [
-    {
-      id: 0,
-      title: 'The Path of the Absurd Man',
-      body: 'The absurd man embraces the principles of revolt, freedom, and passion. What does it mean by freedom?',
-      user:  Repo.users[0]
-    },
-    {
-      id: 1,
-      title: 'Re: The Path of the Absurd Man',
-      body: 'He means leaving without the appeal.',
-      user:  Repo.users[1]
-    },
+    messages: [
+      {
+        id: 0,
+        title: 'The Path of the Absurd Man',
+        body: 'The absurd man embraces the principles of revolt, freedom, and passion. What does it mean by freedom?',
+        user:  this.state.users[0]
+      },
+      {
+        id: 1,
+        title: 'Re: The Path of the Absurd Man',
+        body: 'He means leaving without the appeal.',
+        user:  this.state.users[1]
+      },
 
-    {
-      id: 2,
-      title: 'Virtue as the Mean',
-      body: 'Does he mean it is not a goal or that it is intermediate?',
-      user:  Repo.users[0]
-    },
-    {
-      id: 3,
-      title: 'Re: Virtue as the Mean',
-      body: 'He means it as "intermediate", a virtue lies between excess and defect',
-      user:  Repo.users[2]
-    },
-    {
-      id: 4,
-      title: 'Draft Message',
-      body: 'Draft Message Body',
-      user:  Repo.users[0]
-    }
-  ];
+      {
+        id: 2,
+        title: 'Virtue as the Mean',
+        body: 'Does he mean it is not a goal or that it is intermediate?',
+        user:  this.state.users[0]
+      },
+      {
+        id: 3,
+        title: 'Re: Virtue as the Mean',
+        body: 'He means it as "intermediate", a virtue lies between excess and defect',
+        user:  this.state.users[2]
+      },
+      {
+        id: 4,
+        title: 'Draft Message',
+        body: 'Draft Message Body',
+        user:  this.state.users[0]
+      }
+    ]
+  };
 
   conversations(folder: string): Observable<Conversation[]> {
-    return of(Repo.conversations.filter(c => c.folder === folder));
+    return of(this.state.conversations.filter(c => c.folder === folder));
   }
 
   conversation(id: number): Observable<Conversation> {
-    return of(Repo.conversations[id]);
+    return of(this.state.conversations[id]);
   }
 
   messageTitles(ids: number[]): Observable<Message[]> {
-    return of(Repo.messages.
+    return of(this.state.messages.
       filter(m => ids.indexOf(m.id) > -1).
       map(b => ({id: b.id, title: b.title, user: b.user, body: null})));
   }
 
   message(id: number): Observable<Message> {
-    return of(Repo.messages[id]);
+    return of(this.state.messages[id]);
   }
 }
 
