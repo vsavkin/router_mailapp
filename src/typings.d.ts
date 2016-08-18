9dc3521d0f9b2dfcacc2dde40cb0fd9fcb1b64d6
vsavkin <avix1000@gmail.com>
2016-08-17 22:29:35 -0700

    provide patched typings for jasmine

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,9 @@
 /// <reference path="../typings/browser.d.ts" />
 
 declare var module: {id: string};
+
+declare namespace jasmine {
+  interface Matchers {
+    toHaveText(expected: string): void;
+  }
+}
